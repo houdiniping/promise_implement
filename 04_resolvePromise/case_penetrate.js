4148fa8192a519f81983cd787bc730e892d5ddba
weiping.wu <weiping.wu@xuncetech.com>
2022-08-03 18:11:32 +0800

    why penetrate

--- 04_resolvePromise/case_penetrate.js
+++ 04_resolvePromise/case_penetrate.js
@@ -11,3 +11,16 @@ p
 .then(data => {
   console.log('data', data)
 })
+
+// why penetrate
+//
+p.then(null, err => {
+  console.log('err', err)
+})
+
+// 前一个 then 返回的还是上一个 then 的数据
+p.then(data => {
+  console.log('data', data)
+}, err => {
+  console.log('err2', err)
+})
